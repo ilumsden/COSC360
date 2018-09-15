9310a3df356e8695c257165992bdb7c1ce9cba7c
Ian Lumsden <lumsden.ian@gmail.com>
2018-09-15 00:55:41 -0400

    Small test.

--- lab2/ip_parser.c
+++ lab2/ip_parser.c
@@ -111,6 +111,7 @@ void print_data(IP *ip, FILE *stream)
     Dllist tmp;
     dll_traverse(tmp, ip->names)
     {
+        printf("tmp is %p\n", (void*) tmp);
         char *name = (char*) tmp->val.s;
         if (name == NULL || strcmp(name, "") == 0)
         {
