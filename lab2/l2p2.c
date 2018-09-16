a07a4ba62bd148238b69012c15aa630cc618d854
Ian Lumsden <lumsden.ian@gmail.com>
2018-09-16 19:43:46 -0400

    Testing lseek

--- lab2/l2p2.c
+++ lab2/l2p2.c
@@ -16,7 +16,9 @@ int main(int argc, char **argv)
     JRB tmp;
     JRB nil;
     off_t curr_pos = lseek(stream, 0, SEEK_CUR);
+    printf("curr_pos is %d\n", (int) curr_pos);
     off_t eof = lseek(stream, 0, SEEK_END);
+    printf("eof_pos is %d\n", (int) eof);
     while (curr_pos != eof)
     {
         ip = new_ip();
